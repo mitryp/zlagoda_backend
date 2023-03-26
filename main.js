cb2d4492ddc5958797a6700ec98570c11dd0dd88
Artem Novak <artem.novak@ukma.edu.ua>
2023-03-26 18:49:54 +0300

    Complete basic setup
    - Implement serving the public files (index page, images etc.).
    - Add all basic module dependencies.
    - Remove flutter example.

--- main.js
+++ main.js
@@ -1,1 +1,39 @@
-console.log(process.platform);
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// frontend serving setup
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {
+        root: process.env.PUBLIC_DIR,
+    });
+});
+app.use(express.static(process.env.PUBLIC_DIR));
+app.use((req, res) => {
+    res.redirect("/");
+});
+
+// api routes
+// employee
+
+// product
+
+// store_product
+
+// category
+
+// customer_card
+
+// receipt
+
+app.listen(process.env.PORT, () => {
+    console.log(`Start app, listening at http://localhost:${process.env.PORT}`);
+});
